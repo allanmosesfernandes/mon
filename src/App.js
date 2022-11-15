72540de436dc62a07fe9e4ca1a37557a3e36263a
Allan Fernandes <Fernzallan14@gmail.com>
2022-11-15 15:28:34 +0000

    rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Allan'
+        },
+        {
+          name: 'Rollen'
+        },
+        {
+          name: 'Philip'
+        },
+        {
+          name: 'Alcin'
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div>
+        {
+          this.state.monsters.map((monster,index) => {
+            return <h1 key={index}>{monster.name}</h1>
+          })
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+
+export default App
