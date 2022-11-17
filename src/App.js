2de540e8e38b9cf8a15ba0cbd2d576e1103c5d5b
Allan Fernandes <Fernzallan14@gmail.com>
2022-11-17 01:10:01 +0000

    case-insenstive

--- src/App.js
+++ src/App.js
@@ -4,34 +4,43 @@ export class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Allan'
-        },
-        {
-          name: 'Rollen'
-        },
-        {
-          name: 'Philip'
-        },
-        {
-          name: 'Alcin'
-        }
-      ]
-    }
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(() => {
+      return { monsters:users }
+    }))
   }
   render() {
     return (
       <div>
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='Enter your monster name here' 
+          onChange={(event) => {
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString)
+            })
+            console.log(filteredMonsters)
+            this.setState(() => {
+              return {monsters:filteredMonsters}
+            })
+          }}
+        />
         {
-          this.state.monsters.map((monster,index) => {
-            return <h1 key={index}>{monster.name}</h1>
-          })
+            this.state.monsters.map((monster,index) => {
+              return <h1 key={index}>{monster.name}</h1>
+            })
         }
       </div>
     )
-  }
-}
+  }}
 
 
 export default App
